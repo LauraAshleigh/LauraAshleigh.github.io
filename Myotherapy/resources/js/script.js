b5345a139a3d03caf938f52d08670b8986763b7f
Laura Cronin <cronin.laura91@gmail.com>
2019-10-06 15:25:26 +1100

    Optimise images myo site

--- Myotherapy/resources/js/script.js
+++ Myotherapy/resources/js/script.js
@@ -18,37 +18,7 @@ $(document).ready(function(){
     $('.js--scroll-to-start').click(function () {
         $('html, body').animate({scrollTop: $('.js--mind-body-fitness').offset().top}, 1000);
     });
-    
-    /* Animations on scroll */
-//    $('.js--wp-1').waypoint(function(direction) {
-//        $('.js--wp-1').addClass('animated fadeIn');
-//    }, {
-//        offset: '60%;'
-//    });    
-    
-    $('.js--wp-2').waypoint(function(direction) {
-        $('.js--wp-2').addClass('animated pulse');
-    }, {
-        offset: '60%;'
-    });    
-    
-    $('.js--wp-3').waypoint(function(direction) {
-        $('.js--wp-3').addClass('animated pulse');
-    }, {
-        offset: '60%;'
-    });    
-    
-    $('.js--wp-4').waypoint(function(direction) {
-        $('.js--wp-4').addClass('animated pulse');
-    }, {
-        offset: '60%;'
-    });    
-    
-    $('.js--wp-5').waypoint(function(direction) {
-        $('.js--wp-5').addClass('animated fadeInUp');
-    }, {
-        offset: '60%;'
-    });
+
 
     /* Mobile navigation */
     $('.js--nav-icon').click(function(){
